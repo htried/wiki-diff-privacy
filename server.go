c35eaf5b1a75cc9173f4aeb8d735d3d0ff0f9436
htriedman <htriedman-ctr@wikimedia.org>
2021-05-03 16:31:51 -0400

    debuggin

--- server.go
+++ server.go
@@ -6,7 +6,7 @@ import (
 	"html/template"
 	"log"
 	"net/http"
-	"os"
+	// "os"
 
 	_ "github.com/go-sql-driver/mysql"
 
@@ -45,8 +45,8 @@ func Index(w http.ResponseWriter, r *http.Request) {
 
 	// parse the template at index.html
 	// NOTE: SWITCH WHICH OF THESE STATEMENTS IS COMMENTED OUT TO RUN ON CLOUD VPS VS LOCALLY
-	// t, err := template.ParseFiles("templates/index.html") // LOCAL
-	t, err := template.ParseFiles("/etc/diff-privacy-beam/templates/index.html") // CLOUD VPS
+	t, err := template.ParseFiles("templates/index.html") // LOCAL
+	// t, err := template.ParseFiles("/etc/diff-privacy-beam/templates/index.html") // CLOUD VPS
 	if err != nil {
 		log.Print("error parsing template index_go.html: ", err)
 	}
@@ -106,13 +106,13 @@ func PageViews(w http.ResponseWriter, r *http.Request) {
 // get DB connection, bind functions to paths, and start listener
 func main() {
 	// set up logging
-	f, err := os.OpenFile("/home/htriedman/info.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer f.Close()
+	// f, err := os.OpenFile("/home/htriedman/info.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer f.Close()
 
-	log.SetOutput(f)
+	// log.SetOutput(f)
 
 	// connect to the DB
 	db, err = wdp.DBConnection()
