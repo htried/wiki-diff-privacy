6a87a63acf60169ead53a06c1bcb1a76cb43a9d2
htriedman <htriedman-ctr@wikimedia.org>
2021-05-06 11:00:57 -0400

    update update_data.sh to drop one db at a time

--- config/update_data.sh
+++ config/update_data.sh
@@ -12,8 +12,11 @@ ulimit -q 819200
 echo "initializing languages in databases"
 ${ETC_PATH}/resources/init_db
 
+echo "getting rid of old synthetic data"
+${ETC_PATH}/resources/init_db data
+
 echo "conducting private and public counts of entries in database using apache beam"
 ${ETC_PATH}/resources/beam
 
-echo "cleaning up old db resources"
-${ETC_PATH}/resources/clean_db
+echo "getting rid of old days of output counts"
+${ETC_PATH}/resources/clean_db output
